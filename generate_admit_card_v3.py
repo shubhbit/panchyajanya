4c044f389b34e1f80861bc4a97c8cd4f99326778
Shubham Pandey <shubhampandey@Shubhams-MacBook-Air.local>
2022-12-18 02:24:02 +0530

    added below features: 1. Print 4 admit-card per page In case of same name, second check to be done on adhaar number# After school, do second grouping based on class Print 2 copies, one with adhaar#, without adhaar# Banner: पाञ्चजन्य सामान्य ज्ञान प्रतियोगिता - २०२२.

--- generate_admit_card_v3.py
+++ generate_admit_card_v3.py
@@ -4,14 +4,20 @@ import os
 import time
 import string
 import random
+import copy
 
 #################################### CONFIGS ####################################
-PRINT_LEN = 60
+PRINT_LEN = 65
 ROLL_BASE = 19220000
 FILL_CHAR = "*"
-file = "Panchyajanya_latest.xlsx" # change the file name xlsx which you want to read
-output_file = "Admit_Cards"
+# change the file name xlsx which you want to read
+file = "Panchyajanya_latest.xlsx"
+internal_output_file = "Admit_Cards_Internal_Use"
+external_output_file = "Admit_Cards_External_Use"
+BANNER = " पाञ्चजन्य सामान्य ज्ञान प्रतियोगिता - २०२२ "
+BOTTOM = " प्रवेश पत्र "
 #################################### CONFIGS ####################################
+
 
 def random_string(chars):
     N = chars
@@ -20,10 +26,9 @@ def random_string(chars):
     return res
 
 
-columns_we_need = {"Regestration No.": 1, "Name": 2, "Father's Name": 3,
-                   "Gender": 7, "Date of Birth": 8, "Class": 6, "School": 9, "Interest": 12}
-
-
+columns_we_need = {"Registration No.": 1, "Name": 2, "Father's Name": 3,
+                   "Gender": 7, "Date of Birth": 8, "Class": 6, "School": 9,
+                   "Aadhar No. / School ID": 10, "Interest": 12}
 
 
 def get_no_cols_rows(file):
@@ -44,13 +49,13 @@ def if_student_unique(final_data, row):
                     student, final_data[gender][student]))
                 print("New row: {0}\n".format(row))
                 print(
-                    "checking if father name is same as well, will ignore in case father is same\n")
-                if final_data[gender][student]["Father's Name"].lower().strip() == row[columns_we_need["Father's Name"]].lower().strip():
-                    print("Father name same, ignored...\n\n\n")
+                    "checking if Adhaar# is same as well, will ignore in case Adhaar# is same\n")
+                if str(final_data[gender][student]["Aadhar No. / School ID"]).lower().strip() == str(row[columns_we_need["Aadhar No. / School ID"]]).lower().strip():
+                    print("Adhaar# same, ignored...\n\n\n")
                     is_unique = False
                     break
                 else:
-                    print("Father name not same, not ignored...\n\n\n")
+                    print("Adhaar# not same, not ignored...\n\n\n")
                     new_name = "{0}_{1}".format(
                         student, random_string(len(student)))
                     row.pop(columns_we_need["Name"])
@@ -76,7 +81,8 @@ def read_data(sheet_obj):
         else:
             duplicate += 1
     print("##################### Summary #####################")
-    print("total unique students: ", len(final_data['Male'])+len(final_data['Female']))
+    print("total unique students: ", len(
+        final_data['Male'])+len(final_data['Female']))
     print("duplicates removed: ", duplicate)
     return final_data
 
@@ -112,48 +118,81 @@ def group_by_school(student_by_with_role):
         for students in student_by_with_role[gender].values():
             for student in students:
                 for key in student:
+                    student[key]["Name"] = key
                     if student[key]["School"] not in students_by_school:
-                        students_by_school[student[key]["School"]] = [student]
+                        students_by_school[student[key]
+                                           ["School"]] = [student[key]]
                     else:
                         students_by_school[student[key]
-                                           ["School"]].append(student)
+                                           ["School"]].append(student[key])
     return students_by_school
 
 
-def print_new(data_by_school):
+def generate_print_data(data_by_school, template, external=False):
+    output_file = internal_output_file
+    if external:
+        output_file = external_output_file
     if os.path.exists(output_file):
         os.remove(output_file)
-    st = """
-{0}
-{1}
-{2}
+    for school in data_by_school:
+        school_data = data_by_school[school]
+        for student in sorted(school_data, key=lambda x: x['Class']):
+            data_list = []
+            data_list.append(BANNER.center(PRINT_LEN+8, "*"))
+            name_to_print = student["Name"].split("_")[0]
+            data_list.append("Name: {}".format(
+                name_to_print).center(PRINT_LEN, " "))
+            del student["Name"]
+            if external:
+                output_file = external_output_file
+                del student["Aadhar No. / School ID"]
+            sorted_keys = sorted(student)
+            for indx, k in enumerate(sorted_keys):
+                if indx % 2 == 0:
+                    v = student[k]
+                    if k == "Date of Birth":
+                        v = str(v).split(" ")[0]
+                    fi = "{}: {}".format(k, v)
+                    if indx < len(student)-1:
+                        new_key = sorted_keys[indx+1]
+                        new_value = student[new_key]
+                        if new_key == "Date of Birth":
+                            new_value = str(new_value).split(" ")[0]
+                        space = PRINT_LEN//2-len(fi)
+                        se = " {}{}: {}".format(
+                            " "*space, new_key, new_value)
+                    else:
+                        se = ""
+                    temp_d = "{}{}".format(fi.ljust(0), se)
+                    data_list.append(temp_d)
+
+            data_list.append(BOTTOM.center(PRINT_LEN, "*"))
+            with open(output_file, "a") as f:
+                f.write(template.format(*data_list))
+
+
+def print_new(data_by_school):
+    st_internal = """
+{0}\n
+{1}\n
+{2}\n
 {3}
 {4}
 {5}
 {6}
+{7}
 """
-    for school in data_by_school:
-        for student in data_by_school[school]:
-            data_list = []
-            for name in student:
-                data_list.append("Panchyajanya".center(PRINT_LEN, FILL_CHAR))
-                name_to_print = name.split("_")[0]
-                data_list.append("Name: {}".format(name_to_print).center(PRINT_LEN, " "))
-                for indx, k in enumerate(student[name]):
-                    if indx < len(student[name])//2:
-                        v = student[name][k]
-                        if k == "Date of Birth":
-                            v = str(v).split(" ")[0]
-                        fi = "{}: {}".format(k, v)
-                        new_key = list(student[name].keys())[indx+4]
-                        space = PRINT_LEN//2-len(fi)
-                        se = "{}{}: {}".format(
-                            " "*space, new_key, student[name][new_key])
-                        temp_d = "{}{}".format(fi.ljust(0), se)
-                        data_list.append(temp_d)
-                data_list.append("Admit Card".center(PRINT_LEN, FILL_CHAR))
-            with open(output_file, "a") as f:
-                f.write(st.format(*data_list))
+    st_external = """
+{0}\n
+{1}\n
+{2}\n
+{3}\n
+{4}\n
+{5}
+{6}
+"""
+    generate_print_data(copy.deepcopy(data_by_school), st_external, True)
+    generate_print_data(copy.deepcopy(data_by_school), st_internal)
 
 
 def main():
@@ -168,6 +207,7 @@ def main():
     print_new(students_by_school)
     print("time took: {} seconds".format(end_time-start_time))
     print("##################### Summary #####################")
+
 
 if __name__ == "__main__":
     main()
